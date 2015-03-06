71cf09217224bdcd0d578bae066f1d1cef2ddba2
Alexander Boström <alexander.bostrom97@hotmail.com>
2015-03-06 20:38:03 +0100

    Added test for user first name, osv corr

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should enter first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  
+  test "Should enter last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+  
+  test "Should enter profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:alexander).profile_name
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "user should have profuile name without space" do
+    user = User.new
+    user.profile_name = "My profile with space"
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("profile name must be formated corrctly.")
+  end
+  
 end
