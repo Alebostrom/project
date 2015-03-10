b2291e7747d86f317529e2139aae2ac631090b28
Alexander Boström <alexander.bostrom97@hotmail.com>
2015-03-10 09:41:52 +0100

    Extra update?

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -37,6 +37,16 @@ class StatusesControllerTest < ActionController::TestCase
 
     assert_redirected_to status_path(assigns(:status))
   end
+  
+  test "should create status for current user when logged in" do
+    sign_in users(:alexander)
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:alexis).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:alexander).id
+  end
 
   test "should show status" do
     get :show, id: @status
@@ -53,12 +63,28 @@ class StatusesControllerTest < ActionController::TestCase
   put :update,id:@status, status: {content: @status.content}
   assert_response :redirect
   assert_redirected_to new_user_session_path
-  end
+end
+
+
 
   test "should update status" do
     sign_in users(:alexander)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+  
+  test "should update status for current user when logged in" do
+    sign_in users(:alexander)
+    patch :update, id: @status, status: { content: @status.content, user_id: users(:alexis).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:alexander).id
+  end
+  
+  test "should not update status if nothing have changed" do
+    sign_in users(:alexander)
+    patch :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:alexander).id
   end
 
   test "should destroy status" do
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+   should have_many(:user_friendships)
+   should have_many(:friends)
+  
   test "Should enter first name" do
     user = User.new
     assert !user.save
@@ -45,4 +48,23 @@ class UserTest < ActiveSupport::TestCase
      user.profile_name = 'alex_1'
      assert user.valid?
   end
+  
+  
+  test "that no error when access a friend list" do
+   assert_nothing_raised do
+    users(:alexander).friends
+   end
+  end
+  
+  test "that creating a friendship on a user work" do
+   users(:alexander).friends << users(:mello)
+   users(:alexander).friends.reload
+   assert users(:alexander).friends.include?(users(:mello))
+  end
+  
+  test "that creating a friendship based on user id and friend id works" do
+   UserFriendship.create user_id: users(:alexander).id, friend_id: users(:mello).id
+   assert users(:alexander).friends.include?(users(:mello))
+  end
+  
 end
